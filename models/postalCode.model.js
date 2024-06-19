e86998a27335a79f08e31529421b072cf5ebc783
pgraca97 <116584573+pgraca97@users.noreply.github.com>
2024-06-19 21:42:52 +0100

    small update on models

--- models/postalCode.model.js
+++ models/postalCode.model.js
@@ -2,16 +2,13 @@ module.exports = (sequelize, DataTypes) => {
     const postalCode = sequelize.define('PostalCode', {
         postalCode: {
             type: DataTypes.STRING,
-            allowNull: false,
             primaryKey: true
         },
         locality: {
             type: DataTypes.STRING,
-            allowNull: false
         },
         country: {
             type: DataTypes.STRING,
-            allowNull: false
         }
     }, {
         tableName: 'postalCode',
